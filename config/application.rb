c940d5fce48509aa38f39466b6a964f80acff01a
sway <40759948+sway9119@users.noreply.github.com>
2023-09-14 20:26:05 +0900

    52_アプリケーションの設定情報を定義する

--- config/application.rb
+++ config/application.rb
@@ -2,21 +2,15 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+
+# config配下のapplication.rbは、すべての環境の設定ファイル。
+# 環境ごとに変更する場合、environments配下に＜環境名＞.rbファイルを編集する。
+
 Bundler.require(*Rails.groups)
 
 module LearnRailsApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo' # アプリケーションやActiveRecordで利用するデフォルトのタイムゾーンを設定。デフォルトは'UTC'
   end
 end
