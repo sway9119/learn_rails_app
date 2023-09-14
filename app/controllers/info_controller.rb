3c13f378a81042b457e6bc99383dc776b446abf1
sway <40759948+sway9119@users.noreply.github.com>
2023-09-14 21:08:57 +0900

    58_開発中の懸念事項をメモする

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -8,4 +8,10 @@ class InfoController < ApplicationController
   def app
     LearnRailsApp::Application.config.appvar
   end
+
+  # 開発中の懸念事項をメモする（'rails notes -a FIXME'で抽出できる）
+  # TODO: あとで追加すべき不足している機能を記す
+  # FIXME: 修正すべき壊れたコードを記す
+  # OPTIMIZE: パフォーマンスに影響を与える最適化すべき箇所を記す
+
 end
