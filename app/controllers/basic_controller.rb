815e685b45092d0f279ebefecfa41691fb62f85f
sway <40759948+sway9119@users.noreply.github.com>
2023-09-16 19:45:04 +0900

    82_XML形式のデータをJSON形式に変更

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class BasicController < ApplicationController
   def name
     render plain: <<-EOS
@@ -22,12 +24,23 @@ class BasicController < ApplicationController
 
   def render_xml
     personal = {'name' => 'Yamada', 'old' => 28}
-    render xml: personal
+    render xml: personalc
   end
 
   def render_json
     personal = {name: "Yamada", old: 28}
     render json: personal
   end
-    
+
+  def xml_to_json
+    # 国立国会図書館サーチのAPIを利用し、「猫」に関する文献を検索
+    # (https://iss.ndl.go.jp/information/api/riyou/)
+    Net::HTTP.start('iss.ndl.go.jp', use_ssl: true) do |http|
+      res = http.get('/api/opensearch?cnt=10&title='+ERB::Util.url_encode('猫')+'&ndc=2&dpid=iss-ndl-opac')
+      # XML形式をハッシュ化してからJSON形式に変換する
+      #（JSON形式をハッシュ化するには、ActiveSupport::JSON.decodeメソッドを使う）
+      render json: Hash.from_xml(res.body)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'basic/render_inline'
   get 'basic/render_xml'
   get 'basic/render_json'
+  get 'basic/xml_to_json'
   get 'info/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
