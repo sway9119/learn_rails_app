f40fa8cb391008f0604469ad89cd965f760f51ea
sway <40759948+sway9119@users.noreply.github.com>
2023-09-16 20:28:35 +0900

    89_拡張子に応じて応答フォーマットを切り替える

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,12 @@ class ArticlesController < ApplicationController
   # GET /articles or /articles.json
   def index
     @articles = Article.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @articles} # /articles.jsonでリクエストが来るとJSON形式に変換＆出力
+      format.xml { render xml: @articles} # /articles.xmlでリクエストが来るとXML形式に変換＆出力
+    end
   end
 
   # GET /articles/1 or /articles/1.json
